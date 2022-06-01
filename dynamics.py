85bd4f7e65bd6cce2db66de84b5064d15393f9a7
Minjun Sung <59441392+mnjnsng@users.noreply.github.com>
2022-06-01 12:09:37 -0500

    dynamics as a class, initial commit

--- dynamics.py
+++ dynamics.py
@@ -1,2 +1,19 @@
-def dynamics(t, x, u, ifMatched, ifUnmatched):
-    pass
+class dynamic_system:
+    def __init__(self,t, x, u, ifMatched, ifUnmatched, dx, fx0, gx0, gxp):
+        self.t = t
+        self.x = x
+        self.u = u
+        self.ifMatched = ifMatched
+        self.ifUnmatched = ifUnmatched
+        self.dx = dx #unmatched uncertainty equation
+        self.fx0 = fx0
+        self.gx0 = gx0
+        self.gxp = gxp
+        
+    def dynamics(self):
+        #TODO: do what Yikun did in dynamics.m
+        pass
+
+    def get_fg(self):
+        return self.fx0, self.gx0, self.gxp
+        
